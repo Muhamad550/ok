c9d2be697ca9a59df79698a56b51d4d0c1f753ac
Muhamad550 <ma8287225@gmail.com>
2025-10-23 18:28:05 +0000

    second try

--- blogging_platform/urls.py
+++ blogging_platform/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('blog.urls', namespace='blog')),  # Replace 'blog' with your app name
+    path('api/', include('blog.urls', namespace='blog')),
+    path('', RedirectView.as_view(url='/api/articles/', permanent=False)),
 ]
